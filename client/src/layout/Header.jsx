001a2376056806aeb13d36a72683f53be2b9c2a8
AvitalHoro <avkahana@g.jct.ac.il>
2024-08-09 14:17:54 +0300

    header&sidebar

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import '../style/Layout.css';
 
 const Header = ( { name } ) => {
     
         return (
             <>
-                <header>
+                <header dir="rtl">
                     <img className="logo" src="logo.png" alt="logo" />
-                    <span className="hello_user">{name} שלום</span>
-                    <button className="profile_button" ></button>
+                    <div className="spacer"></div>
+                    <span className="hello_user">שלום {name? name: "נוסע"}</span>
+                    <img  className="profile_button"  src="./profile.png" alt="פרופיל" />
                 </header>
             </>
         )
